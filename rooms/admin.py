0c0185241d510462a56e31e01abd37b29c5bfa9e
2dowon <dwon424@gmail.com>
2021-01-12 14:59:14 +0900

    Admins 끝(#8)

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -13,10 +14,17 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
@@ -52,6 +60,7 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
         "count_amenities",
         "count_photos",
+        "total_rating",
     )
 
     list_filter = (
@@ -64,6 +73,8 @@ class RoomAdmin(admin.ModelAdmin):
         "city",
         "country",
     )
+
+    raw_id_fields = ("host",)
 
     search_fields = ("=city", "^host__username")
 
@@ -81,4 +92,9 @@ class PhothoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition """
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
--- users/admin.py
+++ users/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
 
+# user admin에서 직접 room을 추가하고 싶은 경우
+# from rooms import models as room_models
+
+# class RoomInline(admin.TabularInline):
+#     model = room_models.Room
+
 
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
 
     """ Custom User Admin """
+
+    # user admin에서 직접 room을 추가하고 싶은 경우
+    # inlines = (RoomInline,)
 
     fieldsets = UserAdmin.fieldsets + (
         (
@@ -23,4 +32,19 @@ class CustomUserAdmin(UserAdmin):
                 )
             },
         ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
     )
